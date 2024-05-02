71840a2f4fd24ea4cfb87e31bec5eb7e46f8db13
Birimankhwe <168661095+Birimankhwe@users.noreply.github.com>
2024-05-02 06:46:56 +0100

    novo

---
+++ processa_contato.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $mensagem = $_POST['mensagem'];
+
+    // Adicione aqui o código para enviar o e-mail, por exemplo, usando a função mail() do PHP
+
+    $to = "aljebebi@gmail.com"; // E-mail do destinatário
+$subject = "Assunto do E-mail"; // Assunto do e-mail
+$message = "Conteúdo da mensagem"; // Conteúdo da mensagem
+
+// Cabeçalhos do e-mail
+$headers = "From: seu_email@example.com\r\n";
+$headers .= "Reply-To: seu_email@example.com\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
+
+// Envio do e-mail
+if (mail($to, $subject, $message, $headers)) {
+    echo "E-mail enviado com sucesso!";
+} else {
+    echo "Falha ao enviar o e-mail.";
+}
+
+    // Redirecionamento após o envio
+    header("Location: prestacao_servicos.php?msg=enviado");
+    
+    exit();
+}
+?>
